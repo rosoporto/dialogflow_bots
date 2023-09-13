5e22c65bd3917a0654978428c423740f76df313d
Aleksey <rosoporto@yandex.ru>
2023-09-13 22:38:58 +0300

    add DialogFlow to VK bot

--- vk_bot.py
+++ vk_bot.py
@@ -4,6 +4,7 @@ import logging
 import random
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
+from dialogflow import detect_intent_texts
 
 
 logger = logging.getLogger(__file__)
@@ -15,6 +16,24 @@ def echo(event, vk_api):
         message=event.text,
         random_id=random.randint(1,1000)
     )
+    
+def vk_message(dialogflow_project_id, vk_group_token):
+        vk_session = vk.VkApi(token=vk_group_token)
+        vk_api = vk_session.get_api()
+        longpoll = VkLongPoll(vk_session)
+        
+        try:            
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                    answer_bot = detect_intent_texts(dialogflow_project_id,
+                                                    event.user_id,
+                                                    event.text)
+                    vk_api.messages.send(
+                        user_id=event.user_id,
+                        message=answer_bot,
+                        random_id=random.randint(1,1000))
+        except Exception as e:
+            logger.exception(e)
 
 
 def main():
@@ -22,15 +41,10 @@ def main():
     logging.basicConfig(format=logging_format, level=logging.INFO)
     
     load_dotenv()
+    project_id = os.environ.get('PROJECT_ID')
     vk_group_token = os.environ.get('VK_GROUP_TOKEN')
-
-    vk_session = vk.VkApi(token=vk_group_token)
-    vk_api = vk_session.get_api()
-    longpoll = VkLongPoll(vk_session)
-
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)  
+    
+    vk_message(project_id, vk_group_token)
 
 
 if __name__ == '__main__':
